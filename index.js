c8da888c6a05a430be8f2f60b244f7b0b02634c7
Yerson <barrioslafont.5@gmail.com>
2023-07-17 19:59:06 -0500

    prueba

---
+++ index.js
@@ -0,0 +1,32 @@
+//Operaciones Sincronas
+/*console.log("inicio programa");
+console.log("Mitad de programa");
+console.log("Fin programa");*/
+
+//Operaciones Síncronas
+/*const funcionAsincrona = (funcion) => {
+  setTimeout(() => {
+    funcion();
+  }, 2000);
+};
+
+const miFuncion = () => {
+  console.log("Ejecutando Operacion...");
+};
+
+console.log("Inicio Pograma");
+funcionAsincrona(miFuncion);
+console.log("Fin Pograma");*/
+
+//Operaciones Sincronas
+/*console.log("inicio programa");
+const numeros = [2, 4, 5, 8, 10];
+
+numeros.forEach(item => {
+  console.log("Ejecutando...#" + item);
+})
+console.log("Fin programa");*/
+
+//Operaciones sincronas con setInterval
+
+
